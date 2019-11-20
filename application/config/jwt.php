deb28450145bc8c2455d255470d512a93f6d4db4
alexputra7 <55419818+alexputra7@users.noreply.github.com>
2019-11-20 16:38:02 +0700

    first commit

---
+++ application/config/jwt.php
@@ -0,0 +1,11 @@
+<?php if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['jwt_key'] = '1q2w3e4r5t6y7u8i9o0p';
+
+/*Generated token will expire in 1 minute for sample code
+* Increase this value as per requirement for production
+*/
+$config['token_timeout'] = 1;
+
+/* End of file jwt.php */
+/* Location: ./application/config/jwt.php */
